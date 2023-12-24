c401b6536e37b8e9417d4ae6521b1b66dad3bb3f
imexh <chandikaranawella@gmail.com>
2023-12-24 19:16:55 +0530

    Adding pages

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,9 @@
 import React from 'react'
 
-export default function Dashboard() {
+export default function Home() {
   return (
     <div>
-      <h1>Dashboard</h1>
+      <h1>Home</h1>
     </div>
   )
 }
