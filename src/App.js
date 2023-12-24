1bd3cb97f1c29a886f2824b3e98431b287db6d96
imexh <chandikaranawella@gmail.com>
2023-12-24 19:16:55 +0530

    Adding pages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
+import React from "react";
+import { BrowserRouter as Route, Router, Routes } from "react-router-dom";
+import './App.css';
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Dashboard from "./pages/Dashboard";
+
 function App() {
   return (
     <div className="App">
-      <h1>Main</h1>
+      <Router>
+        <div>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+          </Routes>
+        </div>
+      </Router>
     </div>
   );
 }
