0d9879c630cb7edb65a987d4db1bc648bbbfa25a
imexh <chandikaranawella@gmail.com>
2024-01-06 19:26:25 +0530

    Added styling

--- src/pages/settings/Settings.js
+++ src/pages/settings/Settings.js
@@ -1,14 +1,17 @@
 import React from 'react'
 import NavBarCommon from '../../components/NavBarCommon'
 import FooterCommon from '../../components/FooterCommon'
+import { Container } from 'react-bootstrap'
 
 export default function Settings() {
   return (
     <div>
       <NavBarCommon />
-      <h1>Settings</h1>
-      <p>Under construction....</p>
-      <FooterCommon />
+      <Container>
+        <h1>Settings</h1>
+        <p>Under construction....</p>
+      </Container>
+      {/* <FooterCommon /> */}
     </div>
   )
 }
