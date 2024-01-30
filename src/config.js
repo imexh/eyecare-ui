6284ea5a9890f6ebd31493f1ba435e922736d81e
imexh <chandikaranawella@gmail.com>
2024-01-30 23:02:47 +0530

    changed backend url

--- src/config.js
+++ src/config.js
@@ -1,3 +1,5 @@
 // const URL = "http://localhost:8080";
-const URL = "https://eyecare-api-k5dr.onrender.com";
+// const URL = "https://eyecare-api-k5dr.onrender.com";
+const URL = "https://eyecare-api-ntk32vnf5q-uc.a.run.app/";
+
 export default URL;
